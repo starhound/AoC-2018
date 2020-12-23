de839b5b87bf2d78ca895ca459ee8ac27189f75b
Wesley <wesleyreid0@gmail.com>
2020-12-23 09:06:31 -0500

    Update day1-pt2.py

--- day1/day1-pt2.py
+++ day1/day1-pt2.py
@@ -2,33 +2,29 @@ raw_data = open('input.txt', 'r')
 
 freqs = raw_data.readlines()
 freq_list = [0]
-has_second = False
+current_freq = 0
+found = False
 
-
-def calculate():
-    current_freq = 0
+while found is not True:
     for freq in freqs:
         sign = freq[:1]
         if sign == '+':
             value = freq[1:]
-            current_freq += int(value)
+            current_freq = current_freq + int(value)
+            print(current_freq)
             if current_freq in freq_list:
                 print("Second Freq: " + str(current_freq))
-                return True
+                found = True
+                break
             else:
                 freq_list.append(current_freq)
         elif sign == '-':
             value = freq[1:]
-            current_freq -= int(value)
+            current_freq = current_freq - int(value)
+            print(current_freq)
             if current_freq in freq_list:
                 print("Second Freq: " + str(current_freq))
-                return True
+                found = True
+                break
             else:
                 freq_list.append(current_freq)
-    return False
-
-
-while True:
-    found = calculate()
-    if found:
-        break
